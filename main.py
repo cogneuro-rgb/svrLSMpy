9663285e1adab361015e43733478831ca6c45898
rohan3412 <114685156+rohan3412@users.noreply.github.com>
2025-01-03 17:19:01 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,48 +4,21 @@ from modules.run_svr_lsm_iteration import run_svr_lsm_iteration
 if __name__ == "__main__":
     base_folder = Path.cwd()  # CURRENT DIRECTORY
     base_folder = base_folder / "symptoms"
-    #DYNAMIC EMOTION
-    symptom_folder = base_folder / 'Dynamic_Emotion'
-    csv_name = "Dynamic_Emotion_scores.csv"
-
-    param_grid = {
-        'C': [30,10],
-        'gamma': [2, 1],
-        'epsilon': [0.1]
-    }
-
-    param_grid = {
-        'C': [10],
-        'gamma': [1],
-        'epsilon': [0.1]
-    }
-    behaviour_name = "Dynamic Emotion Recognition"
-    run_svr_lsm_iteration(symptom_folder=symptom_folder,
-                          csv_name=csv_name,
-                          behaviour_name=behaviour_name,
-                          normalize_vector=True,
-                          do_regress_out_lesion_volume=True,
-                          max_score=100,
-                          min_patient_count=10,
-                          param_grid=param_grid,
-                          n_permutations=3,
-                          alpha=0.05,
-                          n_splits=5)
 
 
 
-
-
-
-
-
-
-    '''symptom_folder = base_folder / 'Dysmetropsia'
+    '''
+    symptom_folder = base_folder / 'Dysmetropsia'
     csv_name = "Dysmetropsia.csv"
 
     param_grid = {
         'C': [50,40,30, 20, 10, 5],
         'gamma': [10, 5, 4, 3, 2, 1],
+        'epsilon': [0.1]
+    }
+    param_grid = {
+        'C': [50],
+        'gamma': [1],
         'epsilon': [0.1]
     }
     behaviour_name = "Dysmetropsia"
@@ -57,7 +30,7 @@ if __name__ == "__main__":
                           max_score=100,
                           min_patient_count=5,
                           param_grid=param_grid,
-                          n_permutations=1000,
+                          n_permutations=1,
                           alpha=0.05,
                           n_splits=5)
 
@@ -65,7 +38,7 @@ if __name__ == "__main__":
 
 
 
-
+    '''
 
 
     symptom_folder = base_folder / 'VAST'
@@ -76,6 +49,12 @@ if __name__ == "__main__":
         'gamma': [10, 5, 4, 3, 2, 1],
         'epsilon': [0.1]
     }
+
+    param_grid = {
+        'C': [50],
+        'gamma': [2],
+        'epsilon': [0.1]
+    }
     behaviour_name = "Aphasia"
     run_svr_lsm_iteration(symptom_folder=symptom_folder,
                           csv_name=csv_name,
@@ -83,9 +62,33 @@ if __name__ == "__main__":
                           normalize_vector=True,
                           do_regress_out_lesion_volume=True,
                           max_score=4,
-                          min_patient_count=5,
+                          min_patient_count="10%", #number or string with or without %
+                          param_grid=param_grid,
+                          n_permutations=1,
+                          alpha=0.05,
+                          n_splits=2,
+                          num_slices=7)
+    '''
+    # DYNAMIC EMOTION
+    symptom_folder = base_folder / 'Dynamic_Emotion'
+    csv_name = "Dynamic_Emotion_scores.csv"
+
+    param_grid = {
+        'C': [50, 40, 30, 20, 10, 5],
+        'gamma': [10, 5, 4, 3, 2, 1],
+        'epsilon': [0.1]
+    }
+
+    behaviour_name = "Dynamic Emotion Recognition"
+    run_svr_lsm_iteration(symptom_folder=symptom_folder,
+                          csv_name=csv_name,
+                          behaviour_name=behaviour_name,
+                          normalize_vector=True,
+                          do_regress_out_lesion_volume=True,
+                          max_score=100,
+                          min_patient_count=10,
                           param_grid=param_grid,
                           n_permutations=1000,
                           alpha=0.05,
                           n_splits=5)
-'''
+    '''
